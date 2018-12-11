98de5da0cdd33970fce51933ab1eae9f4d43d678
Nixinerix <xoamara2@gmail.com>
2018-12-10 21:00:51 -0800

    Spotify query working, still needs structure and other items

--- liri.js
+++ liri.js
@@ -3,14 +3,30 @@ require("dotenv").config();
 var Spotify = require('node-spotify-api');
 
 var keys = require("./keys.js");
-console.log(keys.spotify);
 
-var spotify = new Spotify(keys.spotify);
+var inquirer = require("inquirer");
 
-    spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+let liriCommand = process.argv[2];
+let liriInput = process.argv[3];
+
+
+if (liriCommand === "spotify-this-song") {
+
+    var spotify = new Spotify(keys.spotify);
+
+    spotify.search({ type: 'track', query: liriInput }, function (err, data) {
+
+        let songArtist = data.tracks.items[0].artists[0].name;
+        let songName = data.tracks.items[0].name;
+        let songLink = data.tracks.items[0].preview_url;
+        let songAlbum = data.tracks.items[0].album.name;
+        console.log("The song artist is " + songArtist + ".\r\n");
+        console.log("The song name is " + songName + ".\r\n");
+        console.log("Please click the link to preview your song choice: " + songLink + ".\r\n");
+        console.log("The song is from the album titled " + songAlbum + ".\r\n");
+
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-
-        console.log(data);
     });
+}
